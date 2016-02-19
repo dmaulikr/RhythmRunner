b485483bd791685e583ccfeea1af128c6158d4d2
Johnny Le <Johnny@johnnys-macbook-pro.local>
2016-02-19 12:32:04 -0700

    Project begins

--- RhythmRunner/GameScene.swift
+++ RhythmRunner/GameScene.swift
@@ -1,45 +1,106 @@
 //
 //  GameScene.swift
-//  RhythmRunner
+//  Sample
 //
-//  Created by Johnny Le on 2/17/16.
-//  Copyright (c) 2016 Leviathan. All rights reserved.
+//  Created by Johnny Le on 2/16/16.
+//  Copyright (c) 2016 Johnny Le. All rights reserved.
 //
 
+
+/*
+TODO:
+-Add sprite/ movement
+-Add monsters and collision detection
+-Add touch prompts
+-Add BG Music
+-Add start screen
+-Add life bars
+-Add HUD
+-Add score meter
+-Add options screen?
+*/
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let background = SKSpriteNode(imageNamed: "bg")
+    let background2 = SKSpriteNode(imageNamed: "bg")
+    
+    /*
+    Purpose: Initialize game scene
+    */
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        backgroundColor = SKColor.blackColor()
         
-        self.addChild(myLabel)
+        addBG()
+    }
+    
+    /*
+    Purpose: Create and instantiate background images
+    TODO: Add more scene images. (Sun, clouds, moon, etc.)
+    */
+    func addBG() {
+        background.anchorPoint = CGPointZero
+        background.position = CGPointZero
+        background.zPosition = 2
+        
+        background2.anchorPoint = CGPointZero
+        background2.position = CGPointZero
+        background2.zPosition = 2
+        
+        background.position = CGPoint(x: 0, y: 0)
+        background.size = self.frame.size
+        
+        background2.position = CGPoint(x: size.width, y: 0)
+        background2.size = self.frame.size
+        
+        addChild(background)
+        addChild(background2)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
         
+        //Needs to be removed
         for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+//            let location = touch.locationInNode(self)
+//            
+//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
+    
+    
+    /*
+    Purpose: Game loop for updating the scene
+    */
+    override func update(currentTime: NSTimeInterval) {
         /* Called before each frame is rendered */
+        
+        
+        //Infinite Scrolling Background
+        //Values for speed can be changed, look at creating a function that increases the speed of the scrolling
+        background.position = CGPoint(x: background.position.x - 30, y: background.position.y)
+        background2.position = CGPoint(x: background2.position.x - 30, y: background2.position.y)
+        
+        if (background.position.x < -background.size.width) {
+            background.position = CGPointMake(background2.position.x + background2.size.width, background.position.y)
+        }
+        
+        if (background2.position.x < -background2.size.width) {
+            background2.position = CGPointMake(background.position.x + background.size.width, background2.position.y)
+        }
+        
     }
 }
--- RhythmRunner/GameViewController.swift
+++ RhythmRunner/GameViewController.swift
@@ -1,39 +1,48 @@
 //
 //  GameViewController.swift
-//  RhythmRunner
+//  Sample
 //
-//  Created by Johnny Le on 2/17/16.
-//  Copyright (c) 2016 Leviathan. All rights reserved.
+//  Created by Johnny Le on 2/16/16.
+//  Copyright (c) 2016 Johnny Le. All rights reserved.
 //
 
 import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
+        
+        //        if let scene = GameScene(fileNamed:"GameScene") {
+        //            // Configure the view.
+        //            let skView = self.view as! SKView
+        //            skView.showsFPS = true
+        //            skView.showsNodeCount = true
+        //
+        //            /* Sprite Kit applies additional optimizations to improve rendering performance */
+        //            skView.ignoresSiblingOrder = true
+        //
+        //            /* Set the scale mode to scale to fit the window */
+        //            scene.scaleMode = .AspectFill
+        //
+        //            skView.presentScene(scene)
+        //        }
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return .AllButUpsideDown
@@ -41,12 +50,12 @@ class GameViewController: UIViewController {
             return .All
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
